6ac93cd5a6dcd46cf3b20dc09b0c63288c39dbd1
Erin Green <erin.y.green@gmail.com>
2019-11-20 20:49:25 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   
   render() {
     return (
-      <div classname="App">
+      <div className="App">
         <h1>CatBook</h1>
          {this.handleLoading()}
       </div>
